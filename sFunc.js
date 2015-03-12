0f1498322febd9e99d80cb4f71beccdb52ac2241
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-12 12:28:16 +0800

    Update sFunc.js
    Added 3 cookie functions - sFunc_createCookie, sFunc_getCookie, sFunc_deleteCookie .

--- sFunc.js
+++ sFunc.js
@@ -1,3 +1,13 @@
 function sFunc_createCookie(name,value,expires,path) {
  document.cookie = 'name=value; expires=expires, path=path';
 }
+
+function sFunc_getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function sFunc_deleteCookie(name) {
+  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
