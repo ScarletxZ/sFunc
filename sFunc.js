7de37acb5d320fe033eff83ddc5f1b3d289cc68d
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-15 20:57:38 +0800

    Update sFunc.js
    Added selector

--- sFunc.js
+++ sFunc.js
@@ -4,40 +4,43 @@
 * This work is free. You can redistribute it.
 */
 
-function sFunc_createCookie(name,value,expires,path) {
- document.cookie = 'name=value; expires=expires, path=path';
+function $.createCookie(name,value,expires,path) {
+ today = new Date();
+ expire = new Date();
+ expire.setTime(today.getTime() + 3600000*24*expires);
+ document.cookie = name+"="+escape(value)+ ";expires="+expire.toGMTString()+";path="path;
 }
 
-function sFunc_getCookie(name) {
+function $.getCookie(name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
   if (parts.length == 2) return parts.pop().split(";").shift();
 }
 
-function sFunc_deleteCookie(name) {
-  document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+function $.dumpCookie(name) {
+  $.createCookie(name,"",-1);
 }
 
-function sFunc_parse(obj) {
+function $.parse(obj) {
  JSON.parse(obj);
 }
 
-function sFunc_string(obj) {
+function $.stringify(obj) {
  JSON.stringify(obj);
 }
 
-function sFunc_addListener(obj,listener,capture) {
+function $.addListener(obj,listener,capture) {
  obj.addEventListener(obj, listener, capture);
 }
 
-function sFunc_getID(obj) {
+function $.(obj) {
  document.getElementById(obj);
 }
 
-function sFunc_addClass(elem,newclass) {
+function $.addClass(elem,newclass) {
  elem.classList.add(newclass);
 }
 
-function sFunc_append(obj) {
+function $.append(obj) {
  document.appendChild(obj);
 }
