67d626d0565bc57dcdf63a843447fb3638f415d3
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-30 18:36:26 +0800

    Create stealer.js

---
+++ stealer.js
@@ -0,0 +1,7 @@
+/* 
+* stealer.js
+* this will prevent JS stealer from stealing your JS.
+* Required : sFunc.js, this is an addon / plugin for sFunc. 
+* You can't make this work without sFunc; because there's some unique objects inside it, such as $.appendChild etc.
+*/
+
